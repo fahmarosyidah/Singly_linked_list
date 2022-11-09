93997dfa96d2ad4ef8a8c0dca9a95ea21a834426
fahmarosyidah <115075758+fahmarosyidah@users.noreply.github.com>
2022-11-09 17:06:06 +0700

    Membuat method untuk menambahkan sebuah Node ke dalam list

--- Singly_linked_list/Program.cs
+++ Singly_linked_list/Program.cs
@@ -17,6 +17,20 @@ namespace Singly_linked_list
         {
             START = null;
         }
+
+        /*Method untuk menambahkan sebuah Node ke dalam list*/
+        public void addNode()
+        {
+            int nim;
+            string nm;
+            Console.Write("\nMasukkan nomer Mahasiswa: ");
+            nim = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nMasukkan nama Mahasiswa: ");
+            nm = Console.ReadLine();
+            Node nodeBaru = new Node();
+            nodeBaru.noMhs = nim;
+            nodeBaru.nama = nm;
+        }
     }
     class Program
     { 
